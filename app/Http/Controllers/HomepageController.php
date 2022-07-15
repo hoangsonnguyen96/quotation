fd68ef4e519ef0b8787b2b737e47f916ba7f56ee
Sơn Nguyễn <hoangson.nguyen@mcvnetworks.com>
2022-07-15 17:23:48 +0700

    fix frontend

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -6,18 +6,23 @@ use Illuminate\Http\Request;
 use App\Models\Categories;
 use App\Models\ListQuotation;
 use App\Models\Credentials;
+use App\Models\Banner;
 class HomepageController extends Controller
 {
     //
     public function index()
     {
+        $banners = Banner::all();
+        $banner = Banner::orderBy('id','ASC')->first();
         $credential = Credentials::first();
         $categories = Categories::with(['credential','listQuotations'])->get();
-        return view('welcome', ['categories' => $categories, 'credential' => $credential]);
+        return view('welcome', ['categories' => $categories, 'credential' => $credential, 'banners'=>$banners,'banner' =>$banner]);
     }
 
-    public function category(Request $request){
-
+    public function category(Request $request)
+    {
+        $banners = Banner::all();
+        $banner = Banner::orderBy('id','ASC')->first();
         $categories = Categories::all();
         $category = Categories::where('id',$request->id)->first();
         $credentials = Credentials::where('category_id',$category->id)->get();
@@ -26,21 +31,27 @@ class HomepageController extends Controller
         return view('frontend.category', ['categories' => $categories,
                                           'category' => $category,
                                           'credentials' => $credentials,
-                                          'listQuotation' => $listQuotation,]);
+                                          'listQuotation' => $listQuotation,
+                                          'banners'=>$banners,
+                                          'banner' =>$banner]);
 
     }
 
     public function credentials(Request $request){
-        
+
+        $banners = Banner::all();
+        $banner = Banner::orderBy('id','ASC')->first();
         $credential = Credentials::where('id',$request->id)->first();
         $categories = Categories::all();
-        return view('frontend.credentials', ['categories' => $categories, 'credential' =>$credential]);
+        return view('frontend.credentials', ['categories' => $categories, 'credential' =>$credential, 'banners'=>$banners, 'banner' =>$banner]);
     }
 
     public function quotations(Request $request){
-    
+
+        $banners = Banner::all();
+        $banner = Banner::orderBy('id','ASC')->first();
         $quotation = ListQuotation::where('id',$request->id)->first();
         $categories = Categories::all();
-        return view('frontend.quotations', ['categories' => $categories, 'quotation' =>$quotation]);
+        return view('frontend.quotations', ['categories' => $categories, 'quotation' =>$quotation, 'banners'=>$banners,'banner' =>$banner]);
     }
 }
